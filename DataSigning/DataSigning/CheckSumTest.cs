faf9f3519780d3ffe88b79b2d68c0562b73d0325
Majstrak, A. (Adam) <adam.majstrak@ing.com>
2024-02-17 17:18:29 +0100

    init

---
+++ DataSigning/DataSigning/CheckSumTest.cs
@@ -0,0 +1,40 @@
+//# Private key PKCS8 RSA
+//openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048 
+
+//# Public key
+//openssl rsa -pubout -in private.pem -out public.pem
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataSigning;
+
+[TestClass]
+public class CheckSumTest
+{
+    Crypto crypto = new Crypto();
+
+    [TestMethod]
+    public void GivenPhrase_TextTheSame_ThenGoodValidation()
+    {
+        var signature = crypto.SignData("Text to validate", @".\Keys\private.pem");
+        var resultCheckGood = crypto.VerifyData("Text to validate", signature, @".\Keys\public.pem");
+
+        Assert.IsTrue(resultCheckGood);
+    }
+
+    [TestMethod]
+    public void GivenPhrase_TextDifferent_ThenBadValidation()
+    {
+        var signature = crypto.SignData("Text to validate", @".\Keys\private.pem");
+        var resultCheckBad = crypto.VerifyData("Different text to validate", signature, @".\Keys\public.pem");
+        Assert.IsFalse(resultCheckBad);
+    }
+
+    [TestMethod]
+    public void GivenPhrase_KeyDifferent_ThenBadValidation()
+    {
+        var signature = crypto.SignData("Text to validate", @".\Keys\private2.pem");
+        var resultCheckBad = crypto.VerifyData("Text to validate", signature, @".\Keys\public.pem");
+        Assert.IsFalse(resultCheckBad);
+    }
+}
---
+++ DataSigning/DataSigning/Crypto.cs
@@ -0,0 +1,65 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DataSigning;
+internal class Crypto
+{
+    public string SignData(string message, string privateKeyPath)
+    {
+        var privayeKey = ReadAsymmetricKeyParameter(privateKeyPath);
+
+        //Initialization
+        ISigner sig = SignerUtilities.GetSigner("SHA512withRSA");
+        sig.Init(true, privayeKey);
+
+        //Get bytes from message
+        var bytes = Encoding.UTF8.GetBytes(message);
+
+        //Signing data
+        sig.BlockUpdate(bytes, 0, bytes.Length);
+        byte[] signature = sig.GenerateSignature();
+
+        //Return string from bytes as Base64
+        return Convert.ToBase64String(signature);
+    }
+
+    public bool VerifyData(string originalMessage, string signedMessage, string publicKeyPath)
+    {
+        var rsaPublicKey = GetPublicKeyCryptoProvider(publicKeyPath);
+
+        //Get message from Base64
+        var signedBytes = Convert.FromBase64String(signedMessage);
+
+        //Get bytes from original message
+        var bytesToVerify = new UTF8Encoding().GetBytes(originalMessage);
+
+        //Verify data
+        return rsaPublicKey.VerifyData(bytesToVerify, CryptoConfig.MapNameToOID("SHA512")!, signedBytes);
+    }
+
+    private AsymmetricKeyParameter ReadAsymmetricKeyParameter(string privateKeyPath)
+    {
+        using var fileStream = File.OpenText(privateKeyPath);
+        var pemReader = new PemReader(fileStream);
+        return (AsymmetricKeyParameter)pemReader.ReadObject();
+    }
+
+    private RSACryptoServiceProvider GetPublicKeyCryptoProvider(string publicKeyPath)
+    {
+        var publicKey = File.ReadAllText(publicKeyPath);
+
+        //Read public key params
+        var pr = new PemReader(new StringReader(publicKey));
+        var publicKeyParams = (AsymmetricKeyParameter)pr.ReadObject();
+        var rsaParams = DotNetUtilities.ToRSAParameters((RsaKeyParameters)publicKeyParams);
+
+        //Create provider
+        var rsaPublicKey = new RSACryptoServiceProvider();
+        rsaPublicKey.ImportParameters(rsaParams);
+        return rsaPublicKey;
+    }
+}
